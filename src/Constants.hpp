454b1502d673cd6afc0acd6c2b125f8e52ed884c
Philipp Gehrig <philipp@gehrig.info>
2023-07-17 15:05:09 +0200

    update: TODO, added blocks

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -1,6 +1,6 @@
 // all Constants need to be defined here
 
 // field size is 8x8
-constexpr int height = 7;
-constexpr int width = 7;
+constexpr int board_height = 7;
+constexpr int board_width = 7;
 
