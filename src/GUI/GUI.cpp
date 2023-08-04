63daff6580abb376495b01994c20cdce7d3f45e2
Philipp Gehrig <philipp@gehrig.info>
2023-08-04 22:05:04 +0200

    init: drawBoard() und drawNotPlacedBlocks()

--- src/GUI/GUI.cpp
+++ src/GUI/GUI.cpp
@@ -2,10 +2,22 @@
 
 void GUI::drawBoard()
 {
+    int** board = Board::getBoard();
+    for (int column = 0; column < BOARD_HEIGHT; column++) {
+        for (int row = 0; row < BOARD_WIDTH; row++) {
+            std::cout << board[column][row] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 void GUI::drawNotPlacedBlocks()
 {
+    std::vector<Block> notPlacedBlocks = Board::getNotPlacedBlocks();
+    for (auto block : notPlacedBlocks)
+    {
+        std::cout << " " << std::endl;
+    }
 }
 
 void GUI::drawStartScreen()
