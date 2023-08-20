543cc22a1e83e90b51a739d7219df93033531d22
Philipp Gehrig <philipp@gehrig.info>
2023-08-20 20:18:05 +0200

    updated solver tests

--- test/test.cpp
+++ test/test.cpp
@@ -11,11 +11,11 @@ TEST(SolverTests, test_easy_board)
     Solver solver;
 
     Block startBlock1 = {BlockType::ONEBYONE, 1, 1, BLACK};
-    Block startBlock2 = {BlockType::ONEBYTWO, 1, 2, BLACK};
-    Block startBlock3 = {BlockType::ONEBYTHREE, 1, 3, BLACK};
-    Board::placeBlock(startBlock1, 0,0);
-    Board::placeBlock(startBlock2, 0,1);
-    Board::placeBlock(startBlock3, 0,3);
+    Block startBlock2 = {BlockType::ONEBYTWO, 2, 1, BLACK};
+    Block startBlock3 = {BlockType::ONEBYTHREE, 3, 1, BLACK};
+    Board::placeBlock(startBlock1, 2,4);
+    Board::placeBlock(startBlock2, 3,4);
+    Board::placeBlock(startBlock3, 3,3);
 
     int result = solver.solve();
     EXPECT_EQ(result, 1);
@@ -27,11 +27,11 @@ TEST(SolverTests, test_mdeium_board)
     Solver solver;
 
     Block startBlock1 = {BlockType::ONEBYONE, 1, 1, BLACK};
-    Block startBlock2 = {BlockType::ONEBYTWO, 1, 2, BLACK};
-    Block startBlock3 = {BlockType::ONEBYTHREE, 1, 3, BLACK};
-    Board::placeBlock(startBlock1, 0,0);
-    Board::placeBlock(startBlock2, 0,1);
-    Board::placeBlock(startBlock3, 0,3);
+    Block startBlock2 = {BlockType::ONEBYTWO, 2, 1, BLACK};
+    Block startBlock3 = {BlockType::ONEBYTHREE, 3, 1, BLACK};
+    Board::placeBlock(startBlock1, 2,5);
+    Board::placeBlock(startBlock2, 2,6);
+    Board::placeBlock(startBlock3, 3,5);
 
     int result = solver.solve();
     EXPECT_EQ(result, 1);
@@ -45,9 +45,9 @@ TEST(SolverTests, test_hard_board)
     Block startBlock1 = {BlockType::ONEBYONE, 1, 1, BLACK};
     Block startBlock2 = {BlockType::ONEBYTWO, 1, 2, BLACK};
     Block startBlock3 = {BlockType::ONEBYTHREE, 1, 3, BLACK};
-    Board::placeBlock(startBlock1, 0,0);
-    Board::placeBlock(startBlock2, 0,1);
-    Board::placeBlock(startBlock3, 0,3);
+    Board::placeBlock(startBlock1, 6,5);
+    Board::placeBlock(startBlock2, 6,6);
+    Board::placeBlock(startBlock3, 7,5);
 
     int result = solver.solve();
     EXPECT_EQ(result, 1);
